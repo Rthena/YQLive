bfe03b492b8f20c699345f1134e07f3f9f154efb
Athena <gyqonly@163.com>
2017-08-11 17:01:12 +0800

    删除git文件

---
+++ Server/Server/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Server
+//
+//  Created by Rthena on 2016/12/11.
+//  Copyright © 2016年 Athena. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
